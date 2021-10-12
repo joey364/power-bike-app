45a49cdb73a4e040f03380061904f20cdb37b7db
joey364 <71462893+joey364@users.noreply.github.com>
2021-10-12 14:30:04 +0000

    Setup screens

---
+++ screens/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Cart = () => {
+    return (
+        <View>
+            <Text>Product catalogue screen</Text>
+        </View>
+    )
+}
+
+export default Cart
+
+const styles = StyleSheet.create({})
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import Cart from './Cart'
+import Home from './Home'
+import Login from './Login'
+
+export { Cart, Home, Login }
