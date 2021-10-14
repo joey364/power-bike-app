17e87b4e7b3293eed9cea3473feaf70191b907cf
joey364 <71462893+joey364@users.noreply.github.com>
2021-10-14 03:40:13 +0000

    Create cart contents component

---
+++ components/CartContents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CartContents = () => {
+    return (
+        <View>
+            <Text>Cart contents go here</Text>
+        </View>
+    )
+}
+
+export default CartContents
+
+const styles = StyleSheet.create({})
