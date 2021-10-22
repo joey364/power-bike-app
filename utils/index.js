8cdd57c9a26fe9a8ff6baac776d94023e8aa106c
joey364 <71462893+joey364@users.noreply.github.com>
2021-10-22 14:43:53 +0000

    Define colors object to hold app colors

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primary_light: '#E9E8ED', // primary light
+  neutral: '#A4A3A3', // primary neutral
+  primary_dark:'#151110', // primary dark
+  accent: '#DC3C11', // accent color
+
+  dorado: '#5D4C48',
+  zinnwalditeapprox: '#EABDA6',
+  di_serria: '#D8904F',
+  rolling_stone: '#6F7F88',
+
+}
