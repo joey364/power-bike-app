1525079aab6150f670e5e14287b18cf733adbb48
joey364 <71462893+joey364@users.noreply.github.com>
2021-10-22 14:43:05 +0000

    Add fallback image url

--- components/CheckoutItem.js
+++ components/CheckoutItem.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-import { AntDesign, FontAwesome } from '@expo/vector-icons'
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
 
-const CheckoutItem = ({ name, category, price }) => {
+const CheckoutItem = ({ name, category, price, imageSrc }) => {
   return (
     <View style={styles.container}>
       {/* left  */}
       <View
         style={{
           borderRadius: 15,
-          padding: '0.5em',
-          backgroundColor: '#e9e8ed',
+          padding: "0.5em",
+          backgroundColor: "#e9e8ed",
         }}
       >
         <Image
@@ -20,34 +20,36 @@ const CheckoutItem = ({ name, category, price }) => {
             height: 70,
           }}
           source={{
-            uri: 'https://www.pngmart.com/files/6/Bicycle-Transparent-PNG.png',
+            uri:
+              imageSrc ??
+              "https://www.pngmart.com/files/6/Bicycle-Transparent-PNG.png",
           }}
         />
       </View>
 
       {/* middle text */}
       <View
-        style={{ flexGrow: 1, marginLeft: 20, alignItems: 'space-between' }}
+        style={{ flexGrow: 1, marginLeft: 20, alignItems: "space-between" }}
       >
-        <Text style={{ fontWeight: 500, fontSize: 20, color: '#151110' }}>
+        <Text style={{ fontWeight: 500, fontSize: 20, color: "#151110" }}>
           {name}
         </Text>
-        <Text style={{ fontWeight: 500, fontSize: 18, color: '#e9e8ed' }}>
+        <Text style={{ fontWeight: 500, fontSize: 18, color: "#e9e8ed" }}>
           {category}
         </Text>
-        <Text style={{ fontWeight: 600, fontSize: 22, color: '#dc3c11' }}>
-          $ <Text style={{ color: '#151110' }}>{price}</Text>
+        <Text style={{ fontWeight: 600, fontSize: 22, color: "#dc3c11" }}>
+          $ <Text style={{ color: "#151110" }}>{price}</Text>
         </Text>
       </View>
 
       {/* right */}
-      <View style={{ flexGrow: 1, maxWidth: '20%' }}>
+      <View style={{ flexGrow: 1, maxWidth: "20%" }}>
         <FontAwesome
           style={{
-            alignSelf: 'flex-end',
-            backgroundColor: '#e9e8ed',
+            alignSelf: "flex-end",
+            backgroundColor: "#e9e8ed",
             borderRadius: 10,
-            padding: '0.2em',
+            padding: "0.2em",
           }}
           name="trash-o"
           size={24}
@@ -55,9 +57,9 @@ const CheckoutItem = ({ name, category, price }) => {
         />
         <View
           style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
           <AntDesign name="minuscircle" size={26} color="black" />
@@ -66,18 +68,18 @@ const CheckoutItem = ({ name, category, price }) => {
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default CheckoutItem
+export default CheckoutItem;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     flexGrow: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
     marginBottom: 20,
   },
-})
+});
