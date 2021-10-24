7f894292fa727e0e85113e2420103510647f0b6b
joey364 <71462893+joey364@users.noreply.github.com>
2021-10-24 15:43:51 +0000

    Modify CheckoutItem component layout

--- components/CheckoutItem.js
+++ components/CheckoutItem.js
@@ -7,18 +7,20 @@ import { colors } from '../utils'
 const CheckoutItem = ({ name, category, price, imageSrc }) => {
   return (
     <View style={styles.container}>
-      {/* left  */}
+      {/* left image  */}
       <View
         style={{
-          borderRadius: 15,
-          padding: '0.5em',
+          width: 80,
+          height: 80,
+          padding: '1.0em',
           backgroundColor: colors.primary_light,
+          borderRadius: 10,
         }}
       >
         <Image
           style={{
-            width: 70,
-            height: 70,
+            width: '100%',
+            height: '100%',
           }}
           source={{
             uri:
@@ -28,52 +30,76 @@ const CheckoutItem = ({ name, category, price, imageSrc }) => {
         />
       </View>
 
-      {/* middle text */}
+      {/* rest of the text */}
       <View
-        style={{ flexGrow: 1, marginLeft: 20, alignItems: 'space-between' }}
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between ',
+          alignItems: 'center',
+          flexGrow: 1,
+          maxWidth: '90%',
+        }}
       >
-        <Text
-          style={{
-            fontWeight: 500,
-            fontSize: 20,
-            color: colors.primary_dark,
-          }}
-        >
-          {name}
-        </Text>
-        <Text
-          style={{ fontWeight: 500, fontSize: 18, color: colors.primary_light }}
-        >
-          {category}
-        </Text>
-        <Text style={{ fontWeight: 600, fontSize: 22, color: 'colors.accent' }}>
-          $ <Text style={{ color: colors.primary_dark }}>{price}</Text>
-        </Text>
-      </View>
-
-      {/* right */}
-      <View style={{ flexGrow: 1, maxWidth: '20%' }}>
-        <FontAwesome
-          style={{
-            alignSelf: 'flex-end',
-            backgroundColor: colors.primary_light,
-            borderRadius: 10,
-            padding: '0.2em',
-          }}
-          name="trash-o"
-          size={24}
-          color={colors.accent}
-        />
+        {/* Product details text */}
         <View
           style={{
-            flexDirection: 'row',
+            flexDirection: 'column',
             justifyContent: 'space-between',
-            alignItems: 'center',
+            alignItems: 'flex-start',
+            marginLeft: 10,
           }}
         >
-          <AntDesign name="minuscircle" size={26} color="black" />
-          <Text>1</Text>
-          <AntDesign name="pluscircle" size={26} color={colors.accent} />
+          <Text
+            style={{
+              fontWeight: 500,
+              fontSize: 20,
+              color: colors.primary_dark,
+              marginBottom: 5,
+            }}
+          >
+            {name}
+          </Text>
+          <Text
+            style={{
+              fontWeight: 500,
+              fontSize: 18,
+              color: colors.primary_light,
+            }}
+          >
+            {category}
+          </Text>
+          <Text style={{ fontWeight: 600, fontSize: 22, color: colors.accent }}>
+            $ <Text style={{ color: colors.primary_dark }}>{price}</Text>
+          </Text>
+        </View>
+
+        {/* right */}
+        {/* add/ remove from cart text */}
+        <View style={{ maxWidth: '28%', flexGrow: 1 }}>
+          <FontAwesome
+            style={{
+              alignSelf: 'flex-end',
+              backgroundColor: colors.primary_light,
+              padding: '0.2em',
+              borderRadius: 100,
+            }}
+            name="trash-o"
+            size={22}
+            color={colors.accent}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '100%',
+              paddingVertical: '1em',
+            }}
+          >
+            <AntDesign name="minuscircle" size={24} color="black" />
+            <Text>1</Text>
+            <AntDesign name="pluscircle" size={24} color={colors.accent} />
+          </View>
         </View>
       </View>
     </View>
@@ -85,10 +111,9 @@ export default CheckoutItem
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    flexGrow: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    marginBottom: 20,
+    alignItems: 'center',
+    marginBottom: 10,
   },
 })
